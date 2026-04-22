f3de74449547a6734e0633ed8458bdb863769810
Arthur <arthur@cloudawesome.uk>
2026-04-22 15:55:24 +0100

    Implemented IEnurable<T> to MdTable converter
    Closes #34

---
+++ src/CloudAwesome.Markdown/CloudAwesome.MarkdownMaker.Tests/Converters/CollectionToMdTableConverterTests.cs
@@ -0,0 +1,102 @@
+﻿using CloudAwesome.MarkdownMaker.Converters;
+using FluentAssertions;
+
+namespace CloudAwesome.MarkdownMaker.Tests.Converters;
+
+[TestFixture]
+public class CollectionToMdTableConverterTests
+{
+	[Test]
+	public void CollectionOfObjects_Returns_Valid_Markdown()
+	{
+		var collection = new[]
+		{
+			new TestObject { Name = "Test", Year = 2021, Description = "This is a test", Price = 100.00 },
+			new TestObject { Name = "Test2", Year = 2022, Description = "This is a test2", Price = 200.00 },
+			new TestObject { Name = "Test3", Year = 2023, Description = "This is a test3", Price = 300.00 },
+			new TestObject { Name = "Test4", Year = 2024, Description = "This is a test4", Price = 400.00 }
+		};
+		
+		var expectedResult = 
+			$"| Name | Year | Description | Price | {Environment.NewLine}" +
+			$"|---|---|---|---|{Environment.NewLine}" +
+			$"| Test | 2021 | This is a test | 100.00 | {Environment.NewLine}" +
+			$"| Test2 | 2022 | This is a test2 | 200.00 | {Environment.NewLine}" +
+			$"| Test3 | 2023 | This is a test3 | 300.00 | {Environment.NewLine}" +
+			$"| Test4 | 2024 | This is a test4 | 400.00 | {Environment.NewLine}" +
+			$"{Environment.NewLine}";
+		
+		var result = collection.ToMdTable();
+		result.Should().NotBeNull();
+		
+		var markdown = result.Markdown;
+		
+		markdown.Should().NotBeNullOrEmpty();
+		markdown.Should().Be(expectedResult);
+	}
+
+	[Test] public void ToMdTable_Only_Includes_Properties_With_Values()
+	{
+		var collection = new[]
+		{
+			new TestObject { Name = "Test", Year = 2021, Price = 100.00 },
+			new TestObject { Name = "Test2", Year = 2022, Price = 200.00 },
+			new TestObject { Name = "Test3", Year = 2023, Price = 300.00 },
+			new TestObject { Name = "Test4", Year = 2024, Price = 400.00 }
+		};
+		
+		var expectedResult = 
+			$"| Name | Year | Price | {Environment.NewLine}" +
+			$"|---|---|---|{Environment.NewLine}" +
+			$"| Test | 2021 | 100.00 | {Environment.NewLine}" +
+			$"| Test2 | 2022 | 200.00 | {Environment.NewLine}" +
+			$"| Test3 | 2023 | 300.00 | {Environment.NewLine}" +
+			$"| Test4 | 2024 | 400.00 | {Environment.NewLine}" +
+			$"{Environment.NewLine}";
+		
+		var result = collection.ToMdTable();
+		result.Should().NotBeNull();
+		
+		var markdown = result.Markdown;
+		
+		markdown.Should().NotBeNullOrEmpty();
+		markdown.Should().Be(expectedResult);
+	}
+	
+	[Test]
+	public void Object_Property_With_Null_Value_Is_Included_As_Empty_Column_If_IncludeNullProperties_Is_True()
+	{
+		var collection = new[]
+		{
+			new TestObject { Name = "Test", Year = 2021, Price = 100.00 },
+			new TestObject { Name = "Test2", Year = 2022, Price = 200.00 },
+			new TestObject { Name = "Test3", Year = 2023, Price = 300.00 },
+			new TestObject { Name = "Test4", Year = 2024, Price = 400.00 }
+		};
+		
+		var expectedResult = 
+			$"| Name | Year | Description | Price | {Environment.NewLine}" +
+			$"|---|---|---|---|{Environment.NewLine}" +
+			$"| Test | 2021 | (empty) | 100.00 | {Environment.NewLine}" +
+			$"| Test2 | 2022 | (empty) | 200.00 | {Environment.NewLine}" +
+			$"| Test3 | 2023 | (empty) | 300.00 | {Environment.NewLine}" +
+			$"| Test4 | 2024 | (empty) | 400.00 | {Environment.NewLine}" +
+			$"{Environment.NewLine}";
+		
+		var result = collection.ToMdTable(false);
+		result.Should().NotBeNull();
+		
+		var markdown = result.Markdown;
+		
+		markdown.Should().NotBeNullOrEmpty();
+		markdown.Should().Be(expectedResult);
+	}
+
+	public class TestObject
+	{
+		public string Name { get; set; }
+		public int Year { get; set; }
+		public string? Description { get; set; }
+		public double? Price { get; set; }
+	}
+}
---
+++ src/CloudAwesome.Markdown/CloudAwesome.MarkdownMaker/Converters/CollectionToMdTableConverter.cs
@@ -0,0 +1,73 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace CloudAwesome.MarkdownMaker.Converters;
+
+public static class CollectionToMdTableConverter 
+{
+    public static MdTable ToMdTable<T>(this IEnumerable<T>? collection, bool ignorePropertiesWithOnlyNullValues = true)
+    {
+        var result = new MdTable();
+        
+        if (collection == null)
+        {
+            return result;
+        }
+
+        var items = collection.ToList();
+        if (items.Count == 0)
+        {
+            return result;
+        }
+
+        var properties = typeof(T)
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Where(p => p.CanRead)
+            .ToArray();
+
+        if (ignorePropertiesWithOnlyNullValues)
+        {
+            properties = properties
+                .Where(p => items.Any(item => p.GetValue(item) is not null))
+                .ToArray();
+        }
+
+        if (properties.Length == 0)
+        {
+            return result;
+        }
+
+        result.AddColumns(properties.Select(p => p.Name).ToArray());
+
+        foreach (var item in items)
+        {
+            var rowCells = properties
+                .Select(p => ConvertToString(p.GetValue(item)))
+                .ToArray();
+
+            result.AddRowCells(rowCells);
+        }
+
+        return result;
+    }
+    
+    private static string ConvertToString(object? value)
+    {
+        if (value is null)
+        {
+            return "(empty)";
+        }
+
+        return value switch
+        {
+            double d => d.ToString("0.00", CultureInfo.InvariantCulture),
+            //double? d => d?.ToString("0.00", CultureInfo.InvariantCulture) ?? string.Empty,
+            float f => f.ToString("0.00", CultureInfo.InvariantCulture),
+            //float? f => f?.ToString("0.00", CultureInfo.InvariantCulture) ?? string.Empty,
+            DateTime dt => dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+            DateOnly d => d.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+            IFormattable formattable => formattable.ToString(null, CultureInfo.InvariantCulture),
+            _ => value.ToString() ?? "(empty)"
+        };
+    }
+}
